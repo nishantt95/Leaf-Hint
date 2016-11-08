310e8364725404aeec6e3df21590545ccc765467
Nishant Thapliyal <nishantt95@gmail.com>
2016-11-08 16:34:42 +0530

    Add files via upload

---
+++ terms/default.php
@@ -0,0 +1,143 @@
+<html>
+<style>
+uc
+{
+position:absolute;
+left:16%;
+margin-top:-3px;
+font-family: Helvetica, Arial, lucida grande,tahoma,verdana,arial,sans-serif;font-size:25px;
+}
+log
+{
+position:absolute;
+min-width:100%;
+right:0px;
+top:-5px;
+height:60px;
+padding-bottom:5px;
+color:#ffffff;
+background-color:#1C3892;
+background-image: -webkit-linear-gradient(top,#3D42C5,#1B3072);
+background-image: -moz-linear-gradient(top,#3D42C5,#1B3072);
+
+font-family: Helvetica, Arial, lucida grande,tahoma,verdana,arial,sans-serif;font-size:14px;
+}
+
+.font_title
+{
+margin-top:70px;
+position:absolute;
+font-family: Helvetica, Arial, lucida grande,tahoma,verdana,arial,sans-serif;font-size:20px;
+margin-left:200px;
+font-weight:bold;
+}
+
+.font_title2
+{
+margin-top:20px;
+position:absolute;
+font-family: Helvetica, Arial, lucida grande,tahoma,verdana,arial,sans-serif;font-size:20px;
+margin-left:0px;
+font-weight:bold;
+}
+
+.font_data
+{
+position:absolute;
+margin-left:200px;
+position:absolute;
+margin-top:110px;
+}
+
+.font_data2
+{
+position:absolute;
+margin-left:0px;
+position:absolute;
+margin-top:70px;
+}
+
+</style>
+
+
+<body>
+<fieldset style='border:none;width:1100px;position:absolute;'>
+<font class='font_title' >About Us</font>
+<font class='font_data'>
+<ul>
+<li><a href='default.php' style='text-decoration:none;color:blue;'>Leaf Hint</a> is a search engine where students can search for the information related to their friends and for 
+some unknown student of UPES (University of Petroleum and Energy Studies). We have registered 1,254 B.tech (2013-17) batch automatically and rest of the students can register at 
+main page.</li>
+<br>
+<li>Leaf Hint can be considered as a company or an organisation founded by <a href='https://www.facebook.com/nishant.thapliyal' style='text-decoration:none;color:blue;'>Nishant Thapliyal</a> from
+B.Tech CSE Oil & Gas Informatics. This site was initialized as a search engine but we are trying to upgrade it to another level and hence we have created
+a login system so that it is easier for you to search and also to edit your profile in your own way.</li>
+<br>
+</ul>
+<hr style='height: 1px;border: none;background-color: #DDD9D9;'>
+<font class='font_title2' >Privacy Policy & Terms of Use</font>
+<font class='font_data2'>
+<ul>
+<li>If you are using this site or signing up then you need to know all about our terms and conditions, data usage policy, cookies, sessions, privacy policy and more.
+We are trying to make this site secure so that no one can miss use your data and digital images of users available at this site.</li>
+<br>
+<li>Like many other Web sites, <a href='default.php' style='text-decoration:none;color:blue;'>Leaf Hint</a> makes use of log files. 
+The information inside the log files includes internet protocol 
+( IP ) addresses, type of browser, Internet Service Provider ( ISP ), date/time stamp, referring/exit pages, and number of clicks to analyze trends,
+ administer the site, track users movement around the site, and gather demographic information. IP addresses, and other such information are not 
+ linked to any information that is personally identifiable.</li>
+ <br>
+ <li>
+ If we talk about cookies then <a href='default.php' style='text-decoration:none;color:blue;'>Leaf Hint</a> is not using cookies but when we will integrate our
+ web site with third party vendor like Google, Flipkart, Amazon to deliver digital advertisement to monetize our website then they can use it 
+to store information about visitors preferences, record user-specific information on which pages the user access or visit, customize web page content based on 
+ visitors browser type or other information that the visitor sends via their browser.
+ </li>
+ <br>
+ <li>Right now we are not delivering any kinda advertisement so don't give much stress to cookies and when we will integrate digital advertisement then we may
+or may not inform you. You should consult the respective privacy policies of these third-party ad servers for more detailed information on their practices as well 
+as for instructions about how to opt-out of certain practices. <a href='default.php' style='text-decoration:none;color:blue;'>Leaf Hint</a> privacy policy does not apply to third parties,
+and we cannot control the activities 
+of such other advertisers or web sites.</li>
+<br>
+<li>If we talk about data usage policy then this is a searching site and we deliver your data to others and similarly others data is delivered to you. You
+can control over that data delivery by editing your profile. We also display your digital image (profile picture) and if you don't want to display it you can 
+change it to some unknown image ex: a blank image.</li>
+<br>
+<li>This site have all rights to stop this service at any time without providing any prior notice or warning. If such situation occurs, all data will be deleted
+and at that time you don't have any rights to claim for loss of of your profile and data. We trace your IP address to make your profile and ID secure. If any 
+dispute occurs or any kinda problem arises then decision of site owner will be last and after that no further arguments are allowed.</li>
+<br>
+<li>You can delete you account at any time and all your data will be removed. For that you have to upload your ID card so that we can be sure that this is 
+actually you who want to remove his/her data. You can do it <a href='optout.html' style='text-decoration:none;color:blue' >here</a>.
+If you still have any doubt regarding our terms then you can write us mail at nishantt95@gmail.com and we will try to get back to you as soon as possible. It can
+take upto 45 days to reply at your query. We may or may not answer your mail.</li>
+</ul>
+</font>
+
+</font>
+</fieldset>
+<form method="post" action="redirect.php">
+<log>
+<fieldset style='border:0px;'>
+<br>
+<font size='5'><b><uc>Leaf Hint</uc></b></font>
+<center><div>
+<b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+RollNo:</b><input type='text'  name='roll'  autocomplete='off' required>
+<b>SapId:</b> <input type='text' name='SapId' autocomplete='off' required>
+<input type="submit" value="Sign In">
+</div></center>
+
+</fieldset></log>
+</form>
+
+
+<?php
+?>
+
+</body>
+</html>
